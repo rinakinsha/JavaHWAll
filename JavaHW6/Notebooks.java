c4d19eeb9af1ddead4cee92dc33f0c1aad591812
iryna <rinakinsha@yandex.by>
2022-12-11 21:42:01 +0300

    Solution of task 6-1

--- JavaHW6/Notebooks.java
+++ JavaHW6/Notebooks.java
@@ -1,35 +1,136 @@
 package JavaHW6;
 
 public class Notebooks {
-    String model;
-    Double diagonal;
-    String operatingSystem;
-    Integer capacityRam;
-    String typeVideoCard;
-    String typeProcessor;
-    String typeHardDrive;
-    Integer capacityHardDrive;
-    String color;
-    Integer price;
+    private int id;
+    private int capacityHardDrive;
+    private int capacityRam;
+    private int diagonal;
+    private String operatingSystem;
+    private String color;
+    private String brand;
 
-    public Notebooks(String model, Double diagonal, String operatingSystem, Integer capacityRam, String typeVideoCard,
-            String typeProcessor, String typeHardDrive, Integer capacityHardDrive, String color, Integer price) {
-        this.model = model;
+public Notebooks(int id, int capacityHardDrive, int capacityRam, int diagonal, String operatingSystem, String color,
+            String brand) {
+        this.id = id;
+        this.capacityHardDrive = capacityHardDrive;
+        this.capacityRam = capacityRam;
         this.diagonal = diagonal;
         this.operatingSystem = operatingSystem;
-        this.capacityRam = capacityRam;
-        this.typeVideoCard = typeVideoCard;
-        this.typeProcessor = typeProcessor;
-        this.typeHardDrive = typeHardDrive;
-        this.capacityHardDrive = capacityHardDrive;
         this.color = color;
-        this.price = price;
+        this.brand = brand;
     }
 
-    @Override
-    public String toString() {
-        return String.format("%s, %.1f, %s, %d, %s, %s, %s, %d, %s, %d", model, diagonal, operatingSystem, capacityRam,
-                typeVideoCard, typeProcessor,
-                typeHardDrive, capacityHardDrive, color, price);
-    }
+
+@Override
+public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((brand == null) ? 0 : brand.hashCode());
+    result = prime * result + ((color == null) ? 0 : color.hashCode());
+    result = prime * result + diagonal;
+    result = prime * result + capacityHardDrive;
+    result = prime * result + id;
+    result = prime * result + ((operatingSystem == null) ? 0 : operatingSystem.hashCode());
+    result = prime * result + capacityRam;
+    return result;
+}
+
+@Override
+public boolean equals(Object obj) {
+    if (this == obj)
+        return true;
+    if (obj == null)
+        return false;
+    if (getClass() != obj.getClass())
+        return false;
+    Notebooks other = (Notebooks) obj;
+    if (brand == null) {
+        if (other.brand != null)
+            return false;
+    } else if (!brand.equals(other.brand))
+        return false;
+    if (color == null) {
+        if (other.color != null)
+            return false;
+    } else if (!color.equals(other.color))
+        return false;
+    if (diagonal != other.diagonal)
+        return false;
+    if (capacityHardDrive != other.capacityHardDrive)
+        return false;
+    if (id != other.id)
+        return false;
+    if (operatingSystem == null) {
+        if (other.operatingSystem != null)
+            return false;
+    } else if (!operatingSystem.equals(other.operatingSystem))
+        return false;
+    if (capacityRam != other.capacityRam)
+        return false;
+    return true;
+}
+
+public int getId() {
+    return id;
+}
+
+public void setId(int id) {
+    this.id = id;
+}
+
+public int getHDDvolume() {
+    return capacityHardDrive;
+}
+
+public void setHDDvolume(int volume) {
+    this.capacityHardDrive = volume;
+}
+
+public int getRAMvolume() {
+    return capacityRam;
+}
+
+public void setRAMvolume(int volume) {
+    this.capacityRam = volume;
+}
+
+public int getDisplSize() {
+    return diagonal;
+}
+
+public void setDisplSize(int size) {
+    this.diagonal = size;
+}
+
+public String getBrand() {
+    return brand;
+}
+
+public void setBrand(String name) {
+    this.brand = name;
+}
+
+public String getColor() {
+    return color;
+}
+
+public void setColor(String color) {
+    this.color = color;
+}
+
+public String getOStype() {
+    return operatingSystem;
+}
+
+public void setOStype(String type) {
+    this.operatingSystem = type;
+}
+
+@Override
+public String toString() {
+    return "{id=" + id + ",  brand=" + brand + ", color=" + color + ", diagonal=" + diagonal + ", capacityHardDrive="
+            + capacityHardDrive + ",  capacityRam=" + capacityRam+",  operatingSystem=" + operatingSystem+ "}";
+}
+
+
 }
--- JavaHW6/Task61.java
+++ JavaHW6/Task61.java
@@ -1,146 +1,63 @@
 package JavaHW6;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
 
 public class Task61 {
+
     public static void main(String[] args) {
-        Map<Integer, LinkedHashMap<String, String>> database = new HashMap<Integer, LinkedHashMap<String, String>>();
-        Notebooks NB1 = new Notebooks("Lenovo IdeaPad3", 15.6, "Windows 11 Home", 8, "Intel UHD Graphics",
-                "Intel Core i3", "SSD", 256, "golden", 1699);
-        Notebooks NB2 = new Notebooks("HP 15s", 15.6, "DOS", 8, "AMD Radeon Vega 7", "AMD Ryzen 5 5500U", "SSD", 512,
-                "silver", 2099);
-        Notebooks NB3 = new Notebooks("HP 255 G9 (5Y3X5EA)", 15.6, "DOS", 8, "AMD Radeon Vega 7", "	AMD Ryzen 5 5625U",
-                "SSD", 512, "silver", 1899);
-        Notebooks NB4 = new Notebooks("Apple MacBook Air 13", 13.3, "macOS", 8, "Apple M2 8-core GPU", "Apple M2",
-                "SSD", 256, "gray", 3999);
-        Notebooks NB5 = new Notebooks("Asus TUF Gaming F15", 15.6, "DOS", 16, "NVIDIA GeForce RTX 3050",
-                "Intel Core i5-11400H", "SSD", 512, "black", 3999);
+        HashSet<Notebooks> notebooks = new HashSet<>();
+        notebooks.add(new Notebooks(0, 256, 8, 15, "Windows 11 Home", "golden", "Lenovo"));
+        notebooks.add(new Notebooks(1, 512, 8, 16, "DOS", "silver", "HP"));
+        notebooks.add(new Notebooks(2, 512, 8, 16, "DOS", "silver", "HP"));
+        notebooks.add(new Notebooks(3, 256, 8, 13, "macOS", "gray", "Apple MacBook"));
+        notebooks.add(new Notebooks(4, 512, 16, 16, "DOS", "black", "Asus"));
+        Filter(userRequest(), notebooks);
 
-        Integer id = 1;
     }
 
-    static Map addInDataBase(Map listik, Notebooks nb, Integer newId) {
-        ArrayList<String> newNb = new ArrayList<String>(Arrays.asList(nb.toString().split(", ")));
-        Map<String, String> info = new LinkedHashMap<String, String>();
-        String[] infoName = { "model", "diagonal", "operatingSystem", "capacityRam", "typeVideoCard", "typeProcessor",
-                "typeHardDrive", "capacityHardDrive", "color", "price" };
-        Integer i = 0;
-        for (String str : newNb) {
-            info.put(infoName[i], str);
-            i++;
-        }
-        listik.put(newId, info);
-        return listik;
+    public static HashMap<String, String> userRequest() {
+        HashMap<String, String> userRequest = new HashMap<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите параметры для поиска: ");
+        System.out.print("Введите тип ОС:");
+        userRequest.put("operatingSystem", scanner.nextLine());
+        System.out.print("Введите цвет корпуса:");
+        userRequest.put("color", scanner.nextLine());
+        System.out.print("Введите производителя:");
+        userRequest.put("brand", scanner.nextLine());
+        System.out.print("Введите минимальный объем жесткого диска:");
+        userRequest.put("capacityHardDrive", scanner.nextLine());
+        System.out.print("Введите минимальный объем оперативной памяти:");
+        userRequest.put("capacityRam", scanner.nextLine());
+        System.out.print("Введите минимальную диагональ дисплея:");
+        userRequest.put("diagonal", scanner.nextLine());
+        scanner.close();
+        return userRequest;
     }
 
-    static void searchNb(Map spisok) {
-        String findModel = "null";
-        String findSystem = "null";
-        Integer findRam = 0;
-        Integer findHard = 0;
-        String findColor = "null";
-        Integer findPrice = 0;
-        Map searchRequirements = new HashMap<>();
-        searchRequirements.put("model", findModel);
-        searchRequirements.put("operatingSystem", findSystem);
-        searchRequirements.put("capacityRam", findRam);
-        searchRequirements.put("capacityHardDrive", findHard);
-        searchRequirements.put("color", findColor);
-        searchRequirements.put("price", findPrice);
-        Map searchRequirementsZero = new HashMap<>();
-        searchRequirementsZero.putAll(searchRequirements);
+    public static void Filter(HashMap<String, String> userRequest, HashSet<Notebooks> catalog) {
+        Boolean flag = true;
+        for (Notebooks nb : catalog) {
+            if (userRequest.get("brand").equals(nb.getBrand()) || userRequest.get("brand") == "") {
+                if (userRequest.get("color").equals(nb.getBrand()) || userRequest.get("color") == "") {
+                    if (userRequest.get("operatingSystem").equals(nb.getBrand())
+                            || userRequest.get("operatingSystem") == "") {
+                        if (nb.getDisplSize() >= Integer.parseInt(userRequest.get("diagonal")) &&
+                                (nb.getHDDvolume() >= Integer.parseInt(userRequest.get("capacityHardDrive"))) &&
+                                (nb.getRAMvolume() >= Integer.parseInt(userRequest.get("capacityRam")))) {
+                            System.out.println(nb);
+                            flag = false;
 
-        Scanner input = new Scanner(System.in);
-
-        
-            System.out.printf("Критерии поиска: ");
-            System.out.println(searchRequirements);
-            System.out.println("Веберите критерий для поиска:\n" +
-                    "1 - модель\n" +
-                    "2 - операционная система\n" +
-                    "3 - ОЗУ (минимальное значение)\n" +
-                    "4 - объем жесткого диска (минимальное значение)\n" +
-                    "5 - цвет\n" +
-                    "6 - цена (минимальное значение)\n" +
-                    "result - вывести список согласно фильтра\n" +
-                    "clear - сбросить фильтр\n" +
-                    "end - завершение работы");
-
-            String comand = input.next();
-
-            switch (comand) {
-                case "end":
-                    input.close();
-                    System.exit(1);
-                case "1":
-                    System.out.println("Введите модель: ");
-                    findModel = input.toString();
-                    searchRequirements.put("model", findModel);
-                    break;
-                case "2":
-                    System.out.println("Введите операционную систему: ");
-                    findSystem = input.toString();
-                    searchRequirements.put("operatingSystem", findSystem);
-                    break;
-                case "3":
-                    System.out.println("Введите объем ОЗУ: ");
-                    findRam = input.nextInt();
-                    searchRequirements.put("capacityRam", findRam);
-                    break;
-                case "4":
-                    System.out.println("Введите объем жесткого диска: ");
-                    findHard = input.nextInt();
-                    searchRequirements.put("capacityHardDrive", findHard);
-                    break;
-                case "5":
-                    System.out.println("Введите цвет: ");
-                    findColor = input.next();
-                    searchRequirements.put("color", findColor);
-                    break;
-                case "6":
-                    System.out.println("Введите цену: ");
-                    findPrice = input.nextInt();
-                    searchRequirements.put("price", findPrice);
-                    break;
-                case "result":
-                    boolean flag = false;
-                    for (int i = 1; i <= spisok.size(); i++) {
-                        if ((((Map) spisok.get(i)).get("model")).equals(findModel)
-                                || findModel == "null") {
-                            if ((((Map) spisok.get(i)).get("operatingSystem")).equals(findSystem)
-                                    || findSystem == "null") {
-                                if (Integer.parseInt(
-                                        ((String) ((Map) spisok.get(i)).get("capacityRam"))) >= findRam
-                                        || findRam == 0) {
-                                    if (Integer.parseInt(
-                                            ((String) ((Map) spisok.get(i)).get("capacityHardDrive"))) >= findHard
-                                            || findHard == 0) {
-                                        if ((((Map) spisok.get(i)).get("color").equals(findColor))
-                                                || findColor == "null") {
-                                            if (Integer.parseInt(
-                                                    ((String) ((Map) spisok.get(i)).get("price"))) >= findPrice
-                                                    || findPrice == 0) {
-                                                System.out.println(spisok.get(i));
-                                                flag = true;
-                                            }
-                                        }
-                                    }
-                                }
-                            }
                         }
                     }
-                
-                    if (!flag) {
-                        System.out.println("Нет ноутбуков, удовлетворяющих требованиям");
-                    }
-                    break;
-                case "clear": 
-                    searchRequirements.putAll(searchRequirementsZero);
-                    break;
-                default:
-                    System.out.println("Введены неверные данные");
-                    break;
-            }        
+                }
+            }
+
+        }
+        if (!flag) {
+            System.out.println("По вашему запросу ничего не найдено.");
         }
     }
-
+}
